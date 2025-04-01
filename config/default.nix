32b70546adff9f940ffbfa08a666474997cb67ca
Robert Timm <mail@rtti.de>
2025-04-01 09:58:59 +0000

    feat: yank highlight

--- config/default.nix
+++ config/default.nix
@@ -310,6 +310,7 @@
       ''
         set rtp+=${colors-rti},${ftplugin-rti}
         colorscheme rti
+        autocmd TextYankPost * silent! lua vim.highlight.on_yank {higroup=(vim.fn['hlexists']('HighlightedyankRegion') > 0 and 'HighlightedyankRegion' or 'IncSearch'), timeout=300}
       '';
   };
 }
