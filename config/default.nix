a4741a85752f52d9274bea3872891a0081724f2b
Robert Timm <mail@rtti.de>
2025-04-02 08:33:50 +0000

    feat: camelCaseMotion key binds

--- config/default.nix
+++ config/default.nix
@@ -40,6 +40,12 @@
 
       # fugitive
       { mode = [ "n" "v" "t" ]; key = "<localleader>gs"; action = "<cmd>vertical botright Git<cr>"; }
+
+      # CamelCaseMotion
+      { mode = [ "n" "v" "t" ]; key = ",w"; action = "<plug>CamelCaseMotion_w"; }
+      { mode = [ "n" "v" "t" ]; key = ",b"; action = "<plug>CamelCaseMotion_b"; }
+      { mode = [ "n" "v" "t" ]; key = ",e"; action = "<plug>CamelCaseMotion_e"; }
+      { mode = [ "n" "v" "t" ]; key = ",ge"; action = "<plug>CamelCaseMotion_ge"; }
     ];
 
     plugins = {
