f53553351d51a797d38448127099ea61e690933e
Robert Timm <mail@rtti.de>
2025-04-23 04:03:22 +0000

    refactor: Move dashboard plugin config to its own file

---
+++ config/dashboard.nix
@@ -0,0 +1,42 @@
+{ ... }:
+
+{
+  plugins.dashboard = {
+    enable = true;
+    settings = {
+      change_to_vcs_root = false;
+
+      hide.statusline = false;
+      hide.tabline = false;
+
+      theme = "doom";
+
+      config = {
+        header = [
+          ""
+          ""
+          "███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗"
+          "████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║"
+          "██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║"
+          "██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║"
+          "██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║"
+          "╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝"
+          ""
+          ""
+        ];
+        center = [
+          { desc = "New File"; icon = " "; action = "new|only"; }
+          { desc = "Recent Files"; icon = " "; action = "Telescope oldfiles"; }
+          { desc = "Find Files"; icon = " "; action = "Telescope find_files"; }
+          { desc = "Exit"; icon = "󰩈 "; action = "exit"; }
+        ];
+
+        footer = [
+          ""
+          ""
+          "Neovim is open source and freely distributable"
+        ];
+      };
+    };
+  };
+}
--- config/default.nix
+++ config/default.nix
@@ -9,6 +9,7 @@
     ./blink.nix
     ./vim-slime.nix
     ./noice.nix
+    ./dashboard.nix
   ];
 
   config = {
@@ -69,43 +70,6 @@
       gitsigns.enable = true;
       fugitive.enable = true;
       illuminate.enable = true;
-
-      dashboard.enable = true;
-      dashboard.settings = {
-        change_to_vcs_root = false;
-
-        hide.statusline = false;
-        hide.tabline = false;
-
-        theme = "doom";
-
-        config = {
-          header = [
-            ""
-            ""
-            "███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗"
-            "████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║"
-            "██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║"
-            "██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║"
-            "██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║"
-            "╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝"
-            ""
-            ""
-          ];
-          center = [
-            { desc = "New File"; icon = " "; action = "new|only"; }
-            { desc = "Recent Files"; icon = " "; action = "Telescope oldfiles"; }
-            { desc = "Find Files"; icon = " "; action = "Telescope find_files"; }
-            { desc = "Exit"; icon = "󰩈 "; action = "exit"; }
-          ];
-
-          footer = [
-            ""
-            ""
-            "Neovim is open source and freely distributable"
-          ];
-        };
-      };
 
       neo-tree = {
         enable = true;
