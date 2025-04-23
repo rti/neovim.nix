e74344add1ddc0222b9993d9c9975e07f5a863ca
Robert Timm <mail@rtti.de>
2025-04-23 04:36:25 +0000

    refactor: Extract remaining keymaps into a separate file

--- config/default.nix
+++ config/default.nix
@@ -4,6 +4,7 @@
 }: {
   imports = [
     ./opts.nix
+    ./keymaps.nix
     ./treesitter.nix
     ./lualine.nix
     ./blink.nix
@@ -20,26 +21,6 @@
   ];
 
   config = {
-    keymaps = [
-      # Vim convenience
-      { mode = [ "n" "v" "t" ]; key = "<localleader>o"; action = "<cmd>only<cr>"; }
-      { mode = [ "n" "v" "t" ]; key = "<localleader>q"; action = "<cmd>q<cr>"; }
-      { mode = [ "n" "v" "t" ]; key = "<localleader>Q"; action = "<cmd>qa!<cr>"; }
-      { mode = [ "n" ]; key = "K"; action = "i<cr><esc>"; }
-
-      # Window navigation
-      { mode = [ "n" "v" ]; key = "<m-h>"; action = "<c-w>h"; }
-      { mode = [ "n" "v" ]; key = "<m-l>"; action = "<c-w>l"; }
-      { mode = [ "n" "v" ]; key = "<m-j>"; action = "<c-w>j"; }
-      { mode = [ "n" "v" ]; key = "<m-k>"; action = "<c-w>k"; }
-
-      { mode = "t"; key = "<m-h>"; action = "<c-\\><c-n><c-w>h"; }
-      { mode = "t"; key = "<m-l>"; action = "<c-\\><c-n><c-w>l"; }
-      { mode = "t"; key = "<m-j>"; action = "<c-\\><c-n><c-w>j"; }
-      { mode = "t"; key = "<m-k>"; action = "<c-\\><c-n><c-w>k"; }
-      { mode = "t"; key = "<esc>"; action = "<c-\\><c-n>"; }
-    ];
-
     plugins = {
       auto-save.enable = true;
       web-devicons.enable = true;
