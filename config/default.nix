32301b277827195b376d8203880c6fe91bfca2d3
Robert Timm <mail@rtti.de>
2025-04-23 03:55:27 +0000

    refactor: Split out blink config to its own file

--- config/default.nix
+++ config/default.nix
@@ -6,6 +6,7 @@
     ./opts.nix
     ./treesitter.nix
     ./lualine.nix
+    ./blink.nix
   ];
 
   config = {
@@ -62,61 +63,6 @@
     ];
 
     plugins = {
-      blink-cmp = {
-        enable = true;
-        settings = {
-          cmdline = {
-            keymap = {
-              "<C-space>" = [ "show" ];
-              "<C-y>" = [ "select_and_accept" ];
-              "<cr>" = [ "accept" "fallback" ];
-              "<C-e>" = [ "cancel" "show" ];
-              "<C-c>" = [ "cancel" ];
-              "<C-n>" = [ "show_and_insert" "select_next" "fallback" ];
-              "<C-p>" = [ "select_prev" "fallback" ];
-              "<Down>" = [ "select_next" "fallback" ];
-              "<Up>" = [ "select_prev" "fallback" ];
-              # "<Tab>" = [ "show" "accept" ];
-            };
-            completion = {
-              ghost_text = {
-                enabled = false;
-              };
-              list.selection = {
-                preselect = false;
-                auto_insert = true;
-              };
-              menu = {
-                auto_show = {
-                  __raw = "function(ctx) return vim.fn.getcmdtype() == ':' end";
-                };
-              };
-            };
-          };
-          completion = {
-            menu.auto_show = true;
-            menu.border = "rounded";
-            documentation.window.border = "rounded";
-            documentation.auto_show = true;
-            documentation.auto_show_delay_ms = 0;
-          };
-          keymap = {
-            "<C-b>" = [ "scroll_documentation_up" "fallback" ];
-            "<C-e>" = [ "cancel" "show" ];
-            "<C-c>" = [ "cancel" ];
-            "<C-f>" = [ "scroll_documentation_down" "fallback" ];
-            "<C-n>" = [ "show_and_insert" "select_next" "fallback" ];
-            "<C-p>" = [ "select_prev" "fallback" ];
-            "<C-space>" = [ "show" "show_documentation" "hide_documentation" ];
-            "<C-y>" = [ "select_and_accept" ];
-            "<cr>" = [ "accept" "fallback" ];
-            "<Down>" = [ "select_next" "fallback" ];
-            "<Up>" = [ "select_prev" "fallback" ];
-            # "<S-Tab>" = [ "snippet_backward" "fallback" ];
-            # "<Tab>" = [ "snippet_forward" "fallback" ];
-          };
-        };
-      };
       # blink-ripgrep.enable = true;
 
       auto-save.enable = true;
