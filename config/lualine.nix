fc372faf85fc18669b0308a6ac9e13e7d92893be
Your Name <you@example.com>
2025-04-24 14:53:39 +0000

    chore: lualine for fugitive

--- config/lualine.nix
+++ config/lualine.nix
@@ -5,7 +5,8 @@
     enable = true;
     settings.options = {
       theme = "rti";
-      disabled_filetypes = [ "neo-tree" "fugitive" ];
+      # disabled_filetypes = [ "neo-tree" "fugitive" ];
+      disabled_filetypes = [ "neo-tree" ];
       component_separators = { left = ""; right = ""; };
       section_separators = { left = ""; right = ""; };
     };
