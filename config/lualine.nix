4c5d637cd995cd7dbd6ca1802ea1763767a41596
Your Name <you@example.com>
2025-05-06 12:02:30 +0000

    style: no rounded corners on statusbar

--- config/lualine.nix
+++ config/lualine.nix
@@ -8,7 +8,7 @@
       # disabled_filetypes = [ "neo-tree" "fugitive" ];
       disabled_filetypes = [ "neo-tree" ];
       component_separators = { left = ""; right = ""; };
-      section_separators = { left = ""; right = ""; };
+      section_separators = { left = ""; right = ""; };
     };
   };
 }
