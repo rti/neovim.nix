fb2cc84d4ef12f41c1e99da3fc2673c86ee6182e
Your Name <you@example.com>
2025-05-23 18:09:08 +0000

    chore: setup pylsp

--- config/lsp.nix
+++ config/lsp.nix
@@ -22,13 +22,25 @@
 
         pylsp = {
           enable = true;
+
+          # jedi is on by default, still, make it explicit here
           settings.plugins.jedi_completion.enabled = true;
           settings.plugins.jedi_definition.enabled = true;
           settings.plugins.jedi_hover.enabled = true;
           settings.plugins.jedi_references.enabled = true;
           settings.plugins.jedi_signature_help.enabled = true;
           settings.plugins.jedi_symbols.enabled = true;
-          settings.plugins.ruff.enabled = true; # linter and code formatter
+
+          # https://nix-community.github.io/nixvim/search/?query=pylsp*enabled
+          # https://inventwithpython.com/blog/2022/11/19/python-linter-comparison-2022-pylint-vs-pyflakes-vs-flake8-vs-autopep8-vs-bandit-vs-prospector-vs-pylama-vs-pyroma-vs-black-vs-mypy-vs-radon-vs-mccabe/
+          settings.plugins.rope.enabled = true; # refactoring code actions
+          settings.plugins.pyflakes.enabled = true; # linter for warnings and errors
+          settings.plugins.pylsp_mypy.enabled = true; # type checker, slow
+          settings.plugins.black.enabled = true; # formatting
+
+          # all in one linter and code formatter
+          # does not support warnings in may 2025
+          #settings.plugins.ruff.enabled = true; 
         };
 
         bashls.enable = true;
