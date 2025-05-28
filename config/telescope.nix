47164984dd0cbd7f661c175c2bca495ba1b7a894
Your Name <you@example.com>
2025-05-28 16:19:57 +0000

    feat(telescrope): ripgrep hidden !.git

--- config/telescope.nix
+++ config/telescope.nix
@@ -1,8 +1,7 @@
-{ pkgs,... }:
+{ pkgs, lib, ... }:
 
 {
   keymaps = [
-    # Telescope
     { mode = [ "n" "v" ]; key = "<localleader>ff"; action = "<cmd>Telescope find_files<cr>"; }
     { mode = [ "n" "v" ]; key = "<localleader>fg"; action = "<cmd>Telescope live_grep<cr>"; }
     { mode = [ "n" "v" ]; key = "<localleader>fb"; action = "<cmd>Telescope oldfiles<cr>"; }
@@ -19,6 +18,23 @@
         prompt_prefix = "   ";
         selection_caret = " ";
         sorting_strategy = "ascending";
+        vimgrep_arguments =
+          let
+            customRipgrepArgs = [
+              "--hidden"
+              "--glob"
+              "!**/.git/*"
+            ];
+          in
+          [
+            (lib.getExe pkgs.ripgrep)
+            "--color=never"
+            "--no-heading"
+            "--with-filename"
+            "--line-number"
+            "--column"
+            "--smart-case"
+          ] ++ customRipgrepArgs;
       };
     };
   };
