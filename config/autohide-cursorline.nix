7014393068a6e0642b13f7e501b3ad302f089920
Your Name <you@example.com>
2025-06-16 12:53:52 +0000

    feat: cursorline in quickfix and neo-tree

--- config/autohide-cursorline.nix
+++ config/autohide-cursorline.nix
@@ -6,8 +6,8 @@
       vim.api.nvim_exec([[
       augroup noclc_active_window_buffer_cursorline
       autocmd!
-      autocmd WinEnter,BufEnter * if &filetype != 'dashboard' && &filetype != 'DiffviewFiles' && &filetype != 'DiffviewFileHistory' | setlocal cursorline | endif
-      autocmd WinLeave,BufLeave * if &filetype != 'dashboard' && &filetype != 'DiffviewFiles' && &filetype != 'DiffviewFileHistory' | setlocal nocursorline | endif
+      autocmd WinEnter,BufEnter * if &filetype != 'dashboard' && &filetype != 'DiffviewFiles' && &filetype != 'DiffviewFileHistory' && &filetype != 'neo-tree' && &filetype != 'quickfix' | setlocal cursorline | endif
+      autocmd WinLeave,BufLeave * if &filetype != 'dashboard' && &filetype != 'DiffviewFiles' && &filetype != 'DiffviewFileHistory' && &filetype != 'neo-tree' && &filetype != 'quickfix' | setlocal nocursorline | endif
       augroup END
       ]], false)
     end
