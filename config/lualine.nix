a5c54b1b0ccb7263d3dd2e71bb2b52e1a5fb213e
Your Name <you@example.com>
2025-09-05 09:42:52 +0000

    feat: lualine tabline

--- config/lualine.nix
+++ config/lualine.nix
@@ -3,12 +3,16 @@
 {
   plugins.lualine = {
     enable = true;
-    settings.options = {
-      theme = "rti";
-      # disabled_filetypes = [ "neo-tree" "fugitive" ];
-      disabled_filetypes = [ "neo-tree" ];
-      component_separators = { left = ""; right = ""; };
-      section_separators = { left = ""; right = ""; };
+    settings = {
+      options = {
+        theme = "rti";
+        # disabled_filetypes = [ "neo-tree" "fugitive" ];
+        disabled_filetypes = [ "neo-tree" ];
+        component_separators = { left = ""; right = ""; };
+        section_separators = { left = ""; right = ""; };
+        always_show_tabline = false;
+      };
+      tabline = { lualine_a = [ "tabs" ]; };
     };
   };
 }
