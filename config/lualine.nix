7c026657e65ac81ffd2c4ea87acb683f78ce8d72
Your Name <you@example.com>
2025-09-24 05:28:43 +0000

    chore(lualine): leanify

--- config/lualine.nix
+++ config/lualine.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 
 {
   plugins.lualine = {
@@ -13,22 +13,21 @@
         always_show_tabline = false;
       };
 
-      tabline = { lualine_a = [ "tabs" ]; };
+      tabline = {
+        lualine_a = [ "tabs" ];
+      };
 
       sections = {
         lualine_b = [
-          {
-            __unkeyed-1 = "branch";
-          }
           {
             __unkeyed-1 = "filename";
             path = 1; # https://github.com/nvim-lualine/lualine.nvim?tab=readme-ov-file#filename-component-options
           }
         ];
-        lualine_c = [
-          "navic"
-        ];
-        # lualine_x = [ "navic" ];
+        lualine_c = [ "navic" ];
+        lualine_x = [{ __unkeyed-1 = null; }];
+        lualine_y = [ "filetype" ];
+        lualine_z = [ "progress" ];
       };
     };
   };
