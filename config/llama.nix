a882f2ed61ba2cf6d65182fad896d04639474802
rti <mail@rtti.de>
2026-03-09 09:11:50 +0000

    fix: llama.vim hanging space

--- config/llama.nix
+++ config/llama.nix
@@ -7,16 +7,20 @@
   ];
 
   extraConfigLua = ''
-    local function disable_llama_debug()
-      vim.cmd([[
-        let g:llama_config.show_info = v:false
-        let g:llama_config.keymap_fim_accept_word = "<C-F>"
-        let g:llama_config.keymap_fim_accept_line = "<C-G>"
-        let g:llama_config.keymap_fim_accept_full = "<S-Tab>"
-      ]])
-    end
+    vim.cmd([[
+      if !exists('g:llama_config')
+        let g:llama_config={}
+      endif
 
-    vim.defer_fn(disable_llama_debug, 1000)
+      let g:llama_config.show_info = v:false
+      let g:llama_config.keymap_fim_accept_word = "<C-F>"
+      let g:llama_config.keymap_fim_accept_line = "<C-G>"
+      let g:llama_config.keymap_fim_accept_full = "<S-Tab>"
+
+      " work around hanging space
+      " https://github.com/ggml-org/llama.vim/blob/a1c8e6e40cb45df59b78add5b65d575386832465/autoload/llama.vim#L359
+      let g:llama_config.keymap_fim_trigger = ""
+    ]])
 
     -- free the CTRL-G keymap in insert mode
     vim.api.nvim_del_keymap("i", "<C-G>s")
