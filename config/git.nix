3a5146f04a9c604a8fab8ba1aff6050e9c70a29b
rti <mail@rtti.de>
2026-03-27 05:23:14 +0000

    chore(git): back to fugitive

--- config/git.nix
+++ config/git.nix
@@ -5,8 +5,11 @@
 
   keymaps = [
     # neogit
-    { mode = [ "n" "v" ]; key = "<localleader>gs"; action = "<cmd>Neogit<cr>"; }
-    { mode = [ "n" "v" ]; key = "<localleader>gl"; action = ":lua require('neogit').action('log', 'log_head', {'--graph', '--decorate'})()<cr>"; }
+    # { mode = [ "n" "v" ]; key = "<localleader>gs"; action = "<cmd>Neogit<cr>"; }
+    # { mode = [ "n" "v" ]; key = "<localleader>gl"; action = ":lua require('neogit').action('log', 'log_head', {'--graph', '--decorate'})()<cr>"; }
+
+    # fugitive
+    { mode = [ "n" "v" ]; key = "<localleader>gs"; action = "<cmd>vert rightbelow G<cr>"; }
 
     # git-conflict
     { mode = [ "n" ]; key = "]x"; action = "<Plug>(git-conflict-next-conflict)"; }
@@ -24,64 +27,72 @@
     # diffview
     { mode = [ "n" "v" ]; key = "<localleader>gd"; action = ":DiffviewOpen "; }
     { mode = [ "n" "v" ]; key = "<localleader>gS"; action = "<cmd>DiffviewOpen HEAD<cr>"; }
-    # { mode = [ "n" "v" ]; key = "<localleader>gl"; action = "<cmd>DiffviewFileHistory<cr>"; }
-    # { mode = [ "n" "v" ]; key = "<localleader>gf"; action = "<cmd>DiffviewFileHistory %<cr>"; }
+    { mode = [ "n" "v" ]; key = "<localleader>gl"; action = "<cmd>DiffviewFileHistory<cr>"; }
+    { mode = [ "n" "v" ]; key = "<localleader>gf"; action = "<cmd>DiffviewFileHistory %<cr>"; }
   ];
 
   plugins = {
     # https://github.com/NeogitOrg/neogit
-    neogit = {
+    # neogit = {
+    #   enable = true;
+    #   settings = {
+    #     disable_builtin_notifications = true;
+    #     disable_commit_confirmation = true;
+    #     disable_context_highlighting = true;
+    #     disable_insert_on_commit = true;
+    #
+    #     graph_style = "kitty";
+    #
+    #     kind = "vsplit";
+    #
+    #     integrations = {
+    #       diffview = true;
+    #     };
+    #
+    #     signs = {
+    #       # { CLOSED, OPENED }
+    #       hunk = [ "" "" ];
+    #       item = [ "" "" ];
+    #       section = [ "" "" ];
+    #     };
+    #
+    #     mappings = {
+    #       popup = {
+    #         "g?" = "HelpPopup";
+    #         "?" = false;
+    #         "q" = false;
+    #       };
+    #       # finder = { "<esc>" = false; };
+    #       # commit_view = { "<esc>" = false; };
+    #
+    #       commit_view = {
+    #         "gf" = "OpenFileInWorktree";
+    #         # "q" = false; # no effect. why?
+    #       };
+    #       commit_editor = {
+    #         # "q" = false; # no effect. why?
+    #       };
+    #     };
+    #
+    #     commit_editor = {
+    #       kind = "vsplit";
+    #       show_staged_diff = true;
+    #       staged_diff_split_kind = "split";
+    #     };
+    #     commit_popup.kind = "split";
+    #     commit_select.kind = "split";
+    #     popup.kind = "split";
+    #     preview_buffer.kind = "split";
+    #     log_view.kind = "split";
+    #   };
+    # };
+
+    fugitive = {
       enable = true;
-      settings = {
-        disable_builtin_notifications = true;
-        disable_commit_confirmation = true;
-        disable_context_highlighting = true;
-        disable_insert_on_commit = true;
+    };
 
-        graph_style = "kitty";
-
-        kind = "vsplit";
-
-        integrations = {
-          diffview = true;
-        };
-
-        signs = {
-          # { CLOSED, OPENED }
-          hunk = [ "" "" ];
-          item = [ "" "" ];
-          section = [ "" "" ];
-        };
-
-        mappings = {
-          popup = {
-            "g?" = "HelpPopup";
-            "?" = false;
-            "q" = false;
-          };
-          # finder = { "<esc>" = false; };
-          # commit_view = { "<esc>" = false; };
-
-          commit_view = {
-            "gf" = "OpenFileInWorktree";
-            # "q" = false; # no effect. why?
-          };
-          commit_editor = {
-            # "q" = false; # no effect. why?
-          };
-        };
-
-        commit_editor = {
-          kind = "vsplit";
-          show_staged_diff = true;
-          staged_diff_split_kind = "split";
-        };
-        commit_popup.kind = "split";
-        commit_select.kind = "split";
-        popup.kind = "split";
-        preview_buffer.kind = "split";
-        log_view.kind = "split";
-      };
+    gitlinker = {
+      enable = true;
     };
 
     # https://github.com/akinsho/git-conflict.nvim
